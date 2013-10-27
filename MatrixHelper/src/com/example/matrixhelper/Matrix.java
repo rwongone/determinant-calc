76c32d7fae36823623557be2da06802e2f12c342
rwongone <richard.wong.com@gmail.com>
2013-10-26 20:49:12 -0400

    fixed determinant method

--- MatrixHelper/src/com/example/matrixhelper/Matrix.java
+++ MatrixHelper/src/com/example/matrixhelper/Matrix.java
@@ -17,14 +17,14 @@ public class Matrix {
 			determinant = det(M, a);
 		}
 	}
-
+	
 	double det(int m, int[][] val) { // this assumes that mat is an nxn matrix
 		double toReturn = 0;
-		if (n < 1) {
+		if (m < 1) {
 			return 0; // mathematically accurate?
-		} else if (n == 1) {
+		} else if (m== 1) {
 			return val[0][0];
-		} else if (n == 2) {
+		} else if (m == 2) {
 			return (val[0][0] * val[1][1])-(val[0][1] * val[1][0]);
 		} else { // expansion by row 1, could later extend to expansion by best row OR column (r/c with most zeroes)
 			int[][] newVals = {{0}};
@@ -48,16 +48,20 @@ public class Matrix {
 					max = rowZeroes;
 					useRow = 1;
 				}
-				if (colZeroes < max) {
+				if (colZeroes > max) {
 					bestCol = i;
 					max = colZeroes;
 					useRow = 0;
+					//System.out.println("Col " + bestCol + " has " + max + " zeroes!");
 				}
 			}
-
+			
+			//System.out.println(bestRow + ", " + bestCol);
+			
 			if (useRow == 1) {
 				for (int i=0; i<m; i++) { // for each element in the best row or column
 					int coefficient = val[bestRow][i];
+					if (coefficient == 0) continue;
 					newVals = new int[m-1][m-1];
 					int newRow = 0;
 					int newCol = 0;
@@ -73,11 +77,13 @@ public class Matrix {
 							newRow++;
 						}
 					}
-					toReturn += coefficient * det(m - 1, newVals);
+					toReturn += coefficient * Math.pow(-1, i+bestRow) * det(m - 1, newVals);
+					//System.out.println(coefficient + " * " + det(m-1, newVals));
 				}
 			} else {
 				for (int i=0; i<m; i++) {
 					int coefficient = val[i][bestCol];
+					if (coefficient == 0) continue;
 					newVals = new int[m-1][m-1];
 					int newRow = 0;
 					int newCol = 0;
@@ -93,7 +99,8 @@ public class Matrix {
 							newCol++;
 						}
 					}
-					toReturn += coefficient * det(m - 1, newVals);
+					toReturn += coefficient * Math.pow(-1, i + bestCol) * det(m - 1, newVals);
+					//System.out.println(coefficient + " * " + det(m-1, newVals));
 				}
 			}
 		}
@@ -103,5 +110,4 @@ public class Matrix {
 	// determinant
 	// RREF form
 	// invertibility, if invertible then inverse
-
 }
