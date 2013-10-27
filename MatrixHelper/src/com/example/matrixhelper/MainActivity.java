8253e9beba4455b7b0d083467b1e82610f3636c6
Nosajool <j_m_l_95@hotmail.com>
2013-10-26 20:03:07 -0400

    col and row drop down

--- MatrixHelper/src/com/example/matrixhelper/MainActivity.java
+++ MatrixHelper/src/com/example/matrixhelper/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import java.util.Vector;
 
@@ -13,6 +15,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		Spinner spinner2=(Spinner)findViewById(R.id.spinner2);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.row_col_array, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,
+				R.array.row_col_array,android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		spinner2.setAdapter(adapter2);
 	}
 
 	public void test(View v) {
