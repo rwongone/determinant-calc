4642d4825002668068bab02c38e79c2744be5c1b
Nosajool <j_m_l_95@hotmail.com>
2013-10-26 21:28:40 -0400

    fixed some bugs

--- MatrixHelper/src/com/example/matrixhelper/MainActivity.java
+++ MatrixHelper/src/com/example/matrixhelper/MainActivity.java
@@ -11,23 +11,23 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Spinner spinner = (Spinner) findViewById(R.id.spinner);
-		Spinner spinner2=(Spinner)findViewById(R.id.spinner2);
-		// Create an ArrayAdapter using the string array and a default spinner layout
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.row_col_array, android.R.layout.simple_spinner_item);
-		ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,
-				R.array.row_col_array,android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
-		spinner2.setAdapter(adapter2);
-	}
+	 protected void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.activity_main);
+	    Spinner spinnerone = (Spinner) findViewById(R.id.spinner);
+	    Spinner spinnertwo=(Spinner)findViewById(R.id.spinner2);
+	    // Create an ArrayAdapter using the string array and a default spinner layout
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+	            R.array.row_col_array, android.R.layout.simple_spinner_item);
+	    ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,
+	        R.array.row_col_array,android.R.layout.simple_spinner_item);
+	    // Specify the layout to use when the list of choices appears
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    // Apply the adapter to the spinner
+	    spinnerone.setAdapter(adapter);
+	    spinnertwo.setAdapter(adapter2);
+	   }
 
 	public void test(View v) {
 		TextView one;
